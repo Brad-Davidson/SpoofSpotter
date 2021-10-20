e1d8bb230d08031b9708344d695a80e58622d2b7
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-10-20 14:41:05 -0400

    unit tests please work, i'm begging

--- karma.conf.js
+++ karma.conf.js
@@ -38,6 +38,17 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+
+      ChromeHeadlessCI: {
+      
+      base: 'ChromeHeadless',
+      
+      flags: ['–no-sandbox']
+      
+      }
+      
+      },
     singleRun: true,
     restartOnFileChange: true
   });

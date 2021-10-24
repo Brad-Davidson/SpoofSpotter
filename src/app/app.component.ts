8607b3a618c964ead0d73a990fa7f579b8fde841
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-10-24 12:56:29 -0400

    added fire base dependencies (i think)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent{
+
   constructor() {}
 }
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBOhyfNZepkNsDsTVetua9sMb516TxTaFU",
+    authDomain: "spoofspotter.firebaseapp.com",
+    projectId: "spoofspotter",
+    storageBucket: "spoofspotter.appspot.com",
+    messagingSenderId: "681812369026",
+    appId: "1:681812369026:web:b51f3b9aba310fa53a5e9b",
+    measurementId: "G-GC9J3B9HC4"
+  },
   production: false
 };
 

76f8e0123c99591726225c7a9154b72ecf251b5b
musena <noah.muse11@gmail.com>
2021-11-11 22:53:17 -0500

    Update training.page.ts
    Added an if statement lol

--- src/app/pages/training/training.page.ts
+++ src/app/pages/training/training.page.ts
@@ -14,11 +14,13 @@ export class TrainingPage implements OnInit {
 
   ngOnInit() {
     let cookieUser = JSON.parse(localStorage.getItem('user'));
+    if(cookieUser){
     this.authSvc.GetUserByEmail(cookieUser.email).subscribe(user =>{
       if(user.length > 0){
         this.user = user[0] as User;
       }
     });
+  }
   }
 
 }

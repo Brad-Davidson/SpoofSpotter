5c2c01f1f11cc18d273776410a14b393711f5642
Brad Davidson <65302404+Brad-Davidson@users.noreply.github.com>
2021-11-26 23:15:40 -0500

    Fixed unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TestBed, waitForAsync } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from 'src/environments/environment';
+import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -9,6 +14,13 @@ describe('AppComponent', () => {
 
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [IonicModule.forRoot(),
+        AngularFireAuthModule,
+        AngularFirestoreModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        RouterTestingModule.withRoutes([])
+    ],
+    providers: [AngularFirestore],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));

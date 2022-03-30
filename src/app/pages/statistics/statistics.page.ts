47cc900f392be21527d2df594376e52b017c8f9c
bentonjc <bentonjc@mail.uc.edu>
2022-03-29 21:55:29 -0400

    Linegraph update
    Updated linegraph functionality
    -changed UI a bit.
    -removed "Category:" from card, only showing the category title now, also centered this.
    -added new colors to pie graph, to be more distinct from one another.

--- src/app/pages/statistics/statistics.page.ts
+++ src/app/pages/statistics/statistics.page.ts
@@ -7,7 +7,8 @@ import { NewsFeedService } from 'src/app/services/news-feed.service';
 import { UserService } from 'src/app/services/user.service';
 import {Chart} from 'chart.js';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { map } from 'highcharts';
+import { getOptions, map } from 'highcharts';
+
 
 
 @Component({
@@ -23,13 +24,15 @@ export class StatisticsPage implements OnInit, AfterViewInit{
 
   public user = {} as User;
   @ViewChild('doughnutCanvas') private doughnutCanvas: ElementRef;
-  @ViewChild('barCanvas') private barCanvas: ElementRef;
+  @ViewChild('lineCanvas') private lineCanvas: ElementRef;
   public stats = [] as Stats[];
+  public statsTrue = [] as Stats[];
   doughnutChart: Chart;
-  barChart: Chart;
+  lineChart: Chart;
   public labels = [] as string[];
   public values = [] as number[];
   public statMap = new Map<string, number>()
+  public trueStatMap = new Map<string, number>()
   public isDataLoaded: boolean = false;
 
  
@@ -47,7 +50,6 @@ export class StatisticsPage implements OnInit, AfterViewInit{
         console.log(this.stats);
         this.buildStatMap();
         console.log(this.statMap);
-        this.barChartMethod();
         this.doughnutChartMethod();
       });
     }
@@ -58,8 +60,28 @@ export class StatisticsPage implements OnInit, AfterViewInit{
         console.log(this.stats);
         this.buildStatMap();
         console.log(this.statMap);
-        this.barChartMethod();
+
         this.doughnutChartMethod();
+      })
+    }
+
+    if(this.user && this.user.UserID){
+      console.log(this.user.UserID)
+      
+      this.userSvc.GetUserTrueStats(this.user.UserID).subscribe(results =>{
+        this.statsTrue = results as Stats[]
+        console.log(this.statsTrue);
+        this.buildTrueStatMap();
+        this.lineChartMethod();
+        
+      });
+    }
+    else{ //delete this later
+      this.userSvc.GetUserTrueStats("Anonymous").subscribe(results =>{
+        this.statsTrue = results as Stats[]
+        this.buildTrueStatMap();
+        this.lineChartMethod();
+        
       })
     }
 
@@ -81,51 +103,75 @@ export class StatisticsPage implements OnInit, AfterViewInit{
         
     
   }
-
-  barChartMethod(){
-    
-    this.barChart = new Chart(this.barCanvas.nativeElement, {
-      type: 'bar',
-      data: {
-        label: "Categories",
-        labels: [...this.statMap.keys()],
-        datasets: [
-          {    
-            label: "# of Incorrect Guesses",
-            data: [...this.statMap.values()],
-            backgroundColor: ['rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 205, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'],
-            borderColor: ['rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
-            'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
-            'rgb(54, 162, 235)',
-            'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'],
-            borderWidth: 1,
-          
-          }
-        ]
-      }, options: {
-        scales:{
-          yAxes:[
-            
-            {label:"# of Incorrect Guesses",
-              ticks:{
-                beginAtZero: true
-              }
-            }
-          ]
-        }
+  buildTrueStatMap(){
+    this.trueStatMap = new Map<string, number>()
+    this.statsTrue.forEach(stat =>{
+   
+      if(this.trueStatMap.has(stat.DateAdded)){
+        let value = this.trueStatMap.get(stat.DateAdded);
+        value = value + 1;
+        this.trueStatMap.set(stat.DateAdded, value); //if the date exists, iterate the value associated with it.
+      }
+      else{
+        this.trueStatMap.set(stat.DateAdded, 1);
       }
     });
   }
 
+  lineChartMethod(){
+    
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels: [...this.trueStatMap.keys()],
+        datasets: [
+          {
+            data: [...this.trueStatMap.values()],
+              label: 'Total Questions',
+              fill: false,
+              lineTension: 2.0,
+              backgroundColor: 'rgba(75,192,192,0.4)',
+              borderColor: 'rgba(75,192,192,1)',
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: 'rgba(75,192,192,1)',
+              pointBackgroundColor: '#fff',
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+              pointHoverBorderColor: 'rgba(220,220,220,1)',
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+          }], 
+              spanGaps: false,
+              
+
+        },
+        options: {
+
+          scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+        
+          plugins: {
+              sort:
+                  {
+                      enable: true,
+                      sortFunction: (a,b) => {/** implement your own sort function here **/}
+                  }
+          }
+        }
+      }
+        })
+      }
+          
+      
 
   doughnutChartMethod(){
     
@@ -136,21 +182,22 @@ export class StatisticsPage implements OnInit, AfterViewInit{
         datasets: [{  
           data: [...this.statMap.values()],
            backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
+            'rgba(21, 93, 236, 0.2)',
+            'rgba(204, 54, 40, 0.2)',
             'rgba(255, 205, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
+            'rgba(75, 252, 192, 0.2)',
             'rgba(54, 162, 235, 0.2)',
             'rgba(153, 102, 255, 0.2)',
-            'rgba(201, 203, 207, 0.2)'
+            'rgba(39, 203, 68, 0.2)'
            ],
-            borderColor: ['rgb(255, 99, 132)',
-            'rgb(255, 159, 64)',
+            borderColor: [
+            'rgb(21, 93, 236)',
+            'rgb(204, 54, 40)',
             'rgb(255, 205, 86)',
-            'rgb(75, 192, 192)',
+            'rgb(75, 252, 192)',
             'rgb(54, 162, 235)',
             'rgb(153, 102, 255)',
-            'rgb(201, 203, 207)'],
+            'rgb(39, 203, 68)'],
         }]
       }
     });
@@ -159,18 +206,11 @@ export class StatisticsPage implements OnInit, AfterViewInit{
 
   ngAfterViewInit(): void {      
   if(this.isDataLoaded = true){
-    this.barChartMethod();
-     this.doughnutChartMethod();
-    this.barChart.update();
-    this.doughnutChart.update();
-    
-    
-    
-    
-    
-    
+    this.lineChartMethod();
+    this.doughnutChartMethod();
+   this.lineChart.update();
+   this.doughnutChart.update();
   }
     }
-     
-    
+
 }
